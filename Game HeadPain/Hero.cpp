91be65702511402cebc8afcbe1e2eece4d903968
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-12-01 12:50:53 +0400

    feature preparaion

--- Game HeadPain/Hero.cpp
+++ Game HeadPain/Hero.cpp
@@ -4,7 +4,7 @@ unsigned char Hero::__mapSym = 'H';
 
 Hero::Hero(unsigned char renSym, ccolor::Color colSym, Coord coord)
 	: Object(__mapSym, renSym, colSym, coord),
-	_crystal_count(0), _lvl_key(false)
+	_inventory{ 0, 0, false }
 {
 }
 
@@ -16,27 +16,68 @@ Hero::Hero(unsigned char renSym, ccolor::Color colSym)
 Hero::~Hero()
 {}
 
+//void Hero::AddItem(Item item)
+//{
+//	switch (item)
+//	{
+//		case Item::crystal:		
+//		case Item::key:
+//
+//		default: break;
+//	}
+//}
+//
+//void Hero::AddItem(Item item, int count)
+//{
+//
+//}
+//
+//void Hero::SetItem(Item item, int count)
+//{
+//
+//}
+
 void Hero::AddCrystal()
 {
-	++_crystal_count;
+	++_inventory.crystal_count;
 }
 
 void Hero::AddCrystal(int count)
 {
-	_crystal_count += count;
+	_inventory.crystal_count += count;
 }
 
 void Hero::SetCrystal(int count)
 {
-	_crystal_count = count;
+	_inventory.crystal_count = count;
+}
+
+void Hero::AddKey()
+{
+	++_inventory.key_count;
+}
+
+void Hero::AddKey(int count)
+{
+	_inventory.key_count += count;
+}
+
+void Hero::SetKey(int count)
+{
+	_inventory.key_count = count;
+}
+
+bool Hero::CheckLvlkey()
+{
+	return _inventory.lvl_key == true ? true : false;
 }
 
 void Hero::GiveLvlKey()
 {
-	_lvl_key = true;
+	_inventory.lvl_key = true;
 }
 
 void Hero::TakeLvlKey()
 {
-	_lvl_key = false;
+	_inventory.lvl_key = false;
 }
