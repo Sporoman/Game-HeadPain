5c8bbf2b722b6c935b91c1d5ef7784df38be7802
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-12-01 12:50:53 +0400

    feature preparaion

--- Game HeadPain/Hero.h
+++ Game HeadPain/Hero.h
@@ -2,14 +2,20 @@
 #define HERO_H
 
 #include "Object.h"
+#include "Inventory.h"
+
+//enum class Item
+//{
+//	crystal = 0,
+//	key = 1,
+//};
 
 class Hero : public Object
 {
 private:
 	static unsigned char __mapSym;
 
-	int _crystal_count;
-	bool _lvl_key;
+	Inventory _inventory;
 
 public:
 	Hero(unsigned char renSym, ccolor::Color colSym, Coord coord);
@@ -19,6 +25,16 @@ public:
 	void AddCrystal();
 	void AddCrystal(int count);
 	void SetCrystal(int count);
+
+	void AddKey();
+	void AddKey(int count);
+	void SetKey(int count);
+
+	//void AddItem(Item item);
+	//void AddItem(Item item, int count);
+	//void SetItem(Item item, int count);
+
+	bool CheckLvlkey();
 	void GiveLvlKey();
 	void TakeLvlKey();
 };
