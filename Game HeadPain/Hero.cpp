245dba546f460a97e7e6819d7098b50abd1573dd
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-12-01 11:50:47 +0400

    + created Hero class

--- Game HeadPain/Hero.cpp
+++ Game HeadPain/Hero.cpp
@@ -1,1 +1,42 @@
 #include "Hero.h"
+
+unsigned char Hero::__mapSym = 'H';
+
+Hero::Hero(unsigned char renSym, ccolor::Color colSym, Coord coord)
+	: Object(__mapSym, renSym, colSym, coord),
+	_crystal_count(0), _lvl_key(false)
+{
+}
+
+Hero::Hero(unsigned char renSym, ccolor::Color colSym)
+	: Hero(renSym, colSym, Coord{0,0})
+{
+}
+
+Hero::~Hero()
+{}
+
+void Hero::AddCrystal()
+{
+	++_crystal_count;
+}
+
+void Hero::AddCrystal(int count)
+{
+	_crystal_count += count;
+}
+
+void Hero::SetCrystal(int count)
+{
+	_crystal_count = count;
+}
+
+void Hero::GiveLvlKey()
+{
+	_lvl_key = true;
+}
+
+void Hero::TakeLvlKey()
+{
+	_lvl_key = false;
+}
--- Game HeadPain/Hero.h
+++ Game HeadPain/Hero.h
@@ -6,14 +6,21 @@
 class Hero : public Object
 {
 private:
-	
+	static unsigned char __mapSym;
+
+	int _crystal_count;
+	bool _lvl_key;
 
 public:
-	Hero(Object object);
+	Hero(unsigned char renSym, ccolor::Color colSym, Coord coord);
+	Hero(unsigned char renSym, ccolor::Color colSym);
 	~Hero();
 
-
-
+	void AddCrystal();
+	void AddCrystal(int count);
+	void SetCrystal(int count);
+	void GiveLvlKey();
+	void TakeLvlKey();
 };
 
 #endif // HERO_H
