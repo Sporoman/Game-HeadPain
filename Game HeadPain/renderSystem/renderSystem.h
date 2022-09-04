82048620602e8b94a9d82361eb776d88d10c0140
Sporoman Nikishin <sporoman_nikishin@rambler.ru>
2022-09-04 22:43:44 +0400

    update buffers system and lots of fixes

--- Game HeadPain/renderSystem/renderSystem.h
+++ Game HeadPain/renderSystem/renderSystem.h
@@ -2,41 +2,47 @@
 #define RENDERSYSTEM_H
 
 #include <Windows.h>
-#include "../colors.h"
-#include "../RenderObject.h"
+#include "RenderObject.h"
+#include "colors.h"
 
 // Feature of the system: 
-// If the object has a black background, it is not rendered(= transparent)
+// If the object has a black background, it is not rendered(= transparent).
+// To remove this feature, delete the check for a black background in the "DrawChar" method (68-69 lines in .cpp)
 
 class RenderSystem
 {
 private:
-	static const int _screenY = 21 + 10; // additional cells
-	static const int _screenX = 40 + 25; //     for hud
+	static const int s_default_screenY = 21;
+	static const int s_default_screenX = 40;
 
-	RenderObject _backBuffer[_screenY][_screenX];
-	RenderObject _screenBuffer[_screenY][_screenX];
+	const int _screenY;
+	const int _screenX;
+
+	RenderObject** _backBuffer;
+	RenderObject** _screenBuffer;
 
 	HANDLE _consoleHandle;
 
 public:
+	RenderSystem(const int screenY, const int screenX);
 	RenderSystem();
+	~RenderSystem();
 
 	void Render();
 	void Clear();
-	void DrawChar(int y, int x, const RenderObject& object);
-	void DrawFrontChar(int y, int x, const RenderObject& object);
-	void DrawBackCharColor(int y, int x, Color backgroundColor);
-	void SendText(int y, int x, const char* text, Color symbolColor = Color::gray, Color backgroundColor = Color::black);
+	void DrawChar(int y, int x, const RenderObject& obj);
+	void DrawFrontChar(int y, int x, const RenderObject& obj);
+	void DrawBkgCharColor(int y, int x, Color bkgColor);
+	void SendText(int y, int x, const char* text, Color symbolColor = Color::gray, Color bkgColor = Color::black);
 
 private:
 	bool CompareBuffers(const RenderObject* buf_1, const RenderObject* buf_2) const;
-	void SetCursor(int Y, int X);
-	void HideCursor();
-	void ShowCursor();
-	void SetColor(Color symbolColor, Color backgroundColor);
+	void SetColor(Color symbolColor, Color bkgColor);
 	void SetColor(Color symbolColor);
 	void SetDefault();
+	void SetCursor(int y, int x);
+	void HideCursor();
+	void ShowCursor();
 };
 
 #endif // !RENDERSYSTEM_H
